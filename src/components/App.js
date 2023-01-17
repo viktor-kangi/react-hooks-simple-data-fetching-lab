ed65b1da39d8d08aa7f7f11a19c45a53a29fa43e
victor <victor.buthia@student.moringaschool.com>
2023-01-17 13:15:01 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+
+import React, {useEffect, useState} from "react";
+
+const API = "https://dog.ceo/api/breeds/image/random"
+
+function App() {
+    const [dogImages, setDogImages] = useState("")
+
+    useEffect(() => {
+        fetch(API)
+        .then((response) => response.json())
+        .then((image) => {
+            setDogImages(image.message)
+        })
+    }, [])
+    if(dogImages === ""){
+        return <p>Loading...</p>
+    }
+    return (
+        <div>
+            <img src={dogImages} alt="A Random Dog" />
+        </div>
+    )
+}
+
+export default App
